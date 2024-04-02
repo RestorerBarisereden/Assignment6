5783bce306f97645cc7b0bed6184078ce19097d7
RestorerBarisereden <restorerzorzor@gmail.com>
2024-04-02 23:39:19 +0100

    first push

---
+++ ass6.js
@@ -0,0 +1,16 @@
+let month = 8
+if (month > 3 && month <= 6){
+    console.log("spring");
+}
+else if(month >= 6 && month <= 9) {
+    console.log("Summer");
+}
+else if(month >= 6 && month <= 12) {
+    console.log("Autumn");
+} 
+else if(month > 10 && month <= 12) {
+    console.log("Winter");
+} 
+else {
+    console.log(Month + "is not part  of 12 months");
+}
